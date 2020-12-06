f116d48ec1aea243e331e36fe5de351721612650
Gurleen Singh <gurleen@wewager.io>
2020-12-06 10:57:23 -0500

    Added bullboard, lineups, locator, half graphics

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,26 @@
 'use strict';
+const serialport = require('serialport')
+const readline = require('@serialport/parser-readline')
 
 module.exports = function (nodecg) {
-
+	const liveData = nodecg.Replicant('liveData')
+	liveData.value = {
+		'clockStr': "10:00",
+		'homeScore': "12",
+		'awayScore': "10",
+		'shotClock': "30"
+	}
+	if(process.platform == "win32") {
+		const port = new Serialport('COM1')
+		const parser = port.pipe(new readline({delimiter: '\x04'}))
+		parser.on('data', (x) => {
+			data = {
+				'clockStr': x.slice(1,6),
+				'homeScore': x.slice(14, 16),
+				'awayScore': x.slice(17,19),
+				'shotClock': x.slice(9, 12)
+			}
+			liveData.value = data
+		})
+	}
 }
